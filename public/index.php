275786333a09aca75963ef8295a95ae27250248b
rdrgzma <rdrgzma@hotmail.com>
2022-09-18 02:07:30 -0300

    login page

--- public/index.php
+++ public/index.php
@@ -1,3 +1,73 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
 
-echo 'index';
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema SaaS</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <!-- Include the above in your HEAD tag -->
+
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href="css/login.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/icone.png" type="image/x-icon">
+
+</head>
+
+<body>
+    <div class="main">
+        <div class="container">
+            <center>
+                <div class="logo-mobile">
+                    <span class="texto-logo-mobile">
+                        <img src="img/logo.png" alt="saas" width="350px">
+                    </span>
+                </div>
+                <div class="middle">
+
+                    <div id="login">
+
+                        <form action="" method="POST">
+
+                            <fieldset class="clearfix">
+
+                                <p><span class="fa fa-user"></span><input type="text" Placeholder="Email ou CPF" required></p> <!-- JS because of IE support; better: placeholder="Username" -->
+                                <p><span class="fa fa-lock"></span><input type="password" Placeholder="Senha" required></p> <!-- JS because of IE support; better: placeholder="Password" -->
+
+                                <div>
+                                    <span style="width:48%; text-align:left;  display: inline-block;"><a class="small-text" href="#">Recuperar Senha</a></span>
+                                    <span style="width:50%; text-align:right;  display: inline-block;"><input type="submit" value="Entrar"></span>
+                                </div>
+
+                            </fieldset>
+                            <div class="clearfix"></div>
+                        </form>
+
+                        <div class="clearfix"></div>
+
+                    </div> <!-- end login -->
+                    <div class="logo">
+                        <span class="texto-logo">
+                            <img src="img/logo.png" alt="saas" width="350px">
+                        </span>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+
+        </div>
+        </center>
+    </div>
+
+    </div>
+
+</body>
+
+</html>
